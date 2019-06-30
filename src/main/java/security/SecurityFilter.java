f317ba7d16f79ede2ab19376087f4428568e5187
John Gomez <silva.johngomes@gmail.com>
2019-06-30 18:03:46 -0300

    filter autenticação

--- src/main/java/security/SecurityFilter.java
+++ src/main/java/security/SecurityFilter.java
@@ -1,12 +1,39 @@
 package security;
 
-import java.util.logging.Filter;
-import java.util.logging.LogRecord;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
-//public class SecurityFilter implements Filter {
-//
-//    @Override
-//    public boolean isLoggable(LogRecord record) {
-//        return false;
-//    }
-//}
+
+@WebFilter("/followers")
+public class SecurityFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        HttpSession session = req.getSession();
+
+        if(session.getAttribute("user") ==  null) {
+           resp.sendRedirect("index.html");
+           return;
+        }
+
+        filterChain.doFilter(servletRequest,servletResponse);
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
